de6da04da6f54a45eb0fd9e95b6112335205bf6e
al375846 <al375846@uji.es>
2023-06-18 19:01:02 +0200

    added pubs and improved desk view

--- desktop.js
+++ desktop.js
@@ -10,6 +10,9 @@ canvas.appendChild( renderer.domElement );
 var loader = new GLTFLoader();
 loader.load( 'desktop.gltf', function ( gltf ) {
         gltf.scene.position.set(0, 0, 0);
+        gltf.scene.rotation.x = 0.3;
+        gltf.scene.rotation.y = 0.95;
+        //gltf.scene.rotation.z = -Math.PI / 10;
         scene.add( gltf.scene );
     }, undefined, function ( error ) {
         console.error( error );
@@ -20,12 +23,15 @@ scene.background = new THREE.Color( 0xffffff );
 var light = new THREE.HemisphereLight( 0xffffff, 0x080820, 1 );
 scene.add( light );
 
-const camera = new THREE.PerspectiveCamera( 75, canvas.clientWidth / canvas.clientHeight, 0.1, 100 );
-camera.position.set(-0.6, 0.6, 0.2);
-camera.lookAt(2, 0, 0.6);
+const camera = new THREE.PerspectiveCamera( 40, canvas.clientWidth / canvas.clientHeight, 0.1, 100 );
+camera.position.z = 1.3;
+camera.position.y = 0.3;
+camera.position.x = 0.25;
 
 var animate = function () {
     requestAnimationFrame( animate );
+
     renderer.render( scene, camera );
 }
+
 animate();

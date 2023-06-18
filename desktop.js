6e4d14fbe29bb3031714e116607982e621a4b61b
al375846 <al375846@uji.es>
2023-06-18 15:33:51 +0200

    improved import

--- desktop.js
+++ desktop.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.153.0/build/three.module.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.153.0/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 var canvas = document.getElementById("canv-div");

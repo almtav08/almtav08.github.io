4623ab67f09778747830c2ff97193a4a79147e36
al375846 <al375846@uji.es>
2023-06-18 16:43:48 +0200

    build added pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, 'index.html'),
+                projects: resolve(__dirname, 'projects.html'),
+                publications: resolve(__dirname, 'publications.html'),
+                contact: resolve(__dirname, 'contact.html'),
+            },
+        },
         outDir: 'docs'
     },
     publicDir: 'public'

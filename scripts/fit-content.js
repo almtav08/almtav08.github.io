87d61efb01524c2eb2e6c9c5f7e52a527add3abd
almtav08 <alexmartinez3107@gmail.com>
2023-10-22 19:07:33 +0200

    improve fitting content

--- scripts/fit-content.js
+++ scripts/fit-content.js
@@ -1,4 +1,4 @@
 const page = document.querySelector('.page');
 const primaryHeader = document.querySelector('.primary-header');
 
-page.setAttribute('style', 'height: calc(100vh - ' + primaryHeader.clientHeight + 'px)');
+page.setAttribute('style', 'height: calc(100vh - ' + primaryHeader.clientHeight + 'px - 1dvh)');

75f1639d1ebd7c2bc222fb101c5f444154c1c67e
almtav08 <alexmartinez3107@gmail.com>
2023-10-28 18:37:25 +0200

    added deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
+    site: 'https://almtav08.github.io',
 });

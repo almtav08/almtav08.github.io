7ef78126f293b2681c85e0a568632d81ed269616
almtav08 <alexmartinez3107@gmail.com>
2023-11-04 16:32:51 +0100

    Add Partytown integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://almtav08.github.io',
+  site: 'https://almtav08.github.io',
+  integrations: [partytown()]
 });

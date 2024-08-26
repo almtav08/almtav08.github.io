146bbb8cd767420c20ad66713b885a558575487a
almtav08 <alexmartinez3107@gmail.com>
2024-08-26 18:36:42 +0200

    Update portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://almtav08.github.io',
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), partytown()]
 });

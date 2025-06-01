6871b8f4c9f4e0597ffb62bb68fd0e9647e53605
almtav08 <alexmartinez3107@gmail.com>
2025-06-01 12:50:22 +0200

    vite config

--- vite.confing.ts
+++ vite.confing.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
 import path from "path";
+import * as node_fs from "fs";
+
+function get_paths_from_tsconfig() {
+  const tsconfig_s = node_fs
+    .readFileSync("./tsconfig.json", "utf-8")
+    .replace(/\/\/.*$/gm, ""); // Removing comments
+  const tsconfig = JSON.parse(tsconfig_s);
+  const aliases = {};
+  for (const [key, value] of Object.entries(tsconfig.compilerOptions.paths)) {
+    aliases[key] = path.resolve(__dirname, value[0]);
+  }
+  return aliases;
+}
 
 export default defineConfig({
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "../src"),
-    },
+    alias: get_paths_from_tsconfig(),
+  },
+  build: {
+    target: "esnext",
   },
 });
